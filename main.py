a1c2b0e29bb56a5fa4092d9da82d3bae18681975
angeljmercado <mercadomilletangel@gmail.com>
2023-04-30 20:14:20 -0700

    Add headers, and total price

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ for filepath in filepaths:
 
     # Adding Rows   
     for index, row in data.iterrows():
-        print(data)
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
@@ -40,6 +39,22 @@ for filepath in filepaths:
         pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+    # adding the total price column
+    total_sum = data["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"The total price is {str(total_sum)}", ln=1)
+
+    # Add the company name
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt="Python")
 
     pdf.output(f"PDFs/Invoice {filename}.pdf")
-    
